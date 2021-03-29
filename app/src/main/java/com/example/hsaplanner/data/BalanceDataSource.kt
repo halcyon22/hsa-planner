f79045ff87862a1fbc05069093fdd5fc657c8d5b
Mike Partridge <halcyon22@gmail.com>
2021-03-28 20:21:06 -0500

    refactor balance load function

--- app/src/main/java/com/example/hsaplanner/data/BalanceDataSource.kt
+++ app/src/main/java/com/example/hsaplanner/data/BalanceDataSource.kt
@@ -26,64 +26,95 @@ class BalanceDataSource(
         var balance = STARTING_BALANCE.minus(MONTHLY_CONTRIBUTION_AMOUNT)
         var expenseIndex = 0
 
-        repeat(12) {
-            val firstOfMonth = today.plusMonths(it.toLong())
+        repeat(12) { monthIndex ->
+            val firstOfMonth = today.plusMonths(monthIndex.toLong())
             balance = balance.plus(MONTHLY_CONTRIBUTION_AMOUNT)
 
             lines.add(
-                BalanceLine(
-                    firstOfMonth,
-                    description = context.getString(R.string.personal_contribution),
-                    transactionAmount = MONTHLY_CONTRIBUTION_AMOUNT,
-                    balance
-                )
+                makePersonalContribution(firstOfMonth, balance)
             )
 
-            if (firstOfMonth.month == Month.JANUARY || firstOfMonth.month == Month.JULY) {
-                balance = balance.plus(EMPLOYER_CONTRIBUTION_AMOUNT)
-                lines.add(
-                    BalanceLine(
-                        firstOfMonth.plusDays(1),
-                        description = context.getString(R.string.employer_contribution),
-                        transactionAmount = EMPLOYER_CONTRIBUTION_AMOUNT,
-                        balance
-                    )
-                )
+            makeEmployerContribution(firstOfMonth, balance)?.also {
+                lines.add(it)
+                balance = it.balance
             }
 
-            if (balance > THRESHOLD_AMOUNT && expenses.size > expenseIndex) {
-                val expense = expenses[expenseIndex]
-                var reimbursementAmount = REIMBURSEMENT_MAX
-                if (expense.remainingAmount > REIMBURSEMENT_MAX) {
-                    expense.remainingAmount = expense.remainingAmount.minus(REIMBURSEMENT_MAX)
-                } else {
-                    reimbursementAmount = expense.remainingAmount
-                    expense.remainingAmount = BigDecimal.ZERO
+            makeReimbursementLines(expenseIndex, firstOfMonth, balance)?.also {
+                lines.add(it.accountLine)
+                lines.add(it.expenseLine)
+                balance = it.endingAccountBalance
+                if (it.expenseFullyReimbursed) {
                     expenseIndex++
                 }
-
-                balance = balance.minus(reimbursementAmount)
-                lines.add(
-                    BalanceLine(
-                        firstOfMonth.plusDays(2),
-                        description = context.getString(R.string.reimbursement),
-                        transactionAmount = reimbursementAmount.negate(),
-                        balance
-                    )
-                )
-
-                lines.add(
-                    BalanceLine(
-                        firstOfMonth.plusDays(3),
-                        description = expense.description,
-                        transactionAmount = reimbursementAmount.negate(),
-                        expense.remainingAmount
-                    )
-                )
             }
         }
 
         return lines
     }
+
+    private fun makePersonalContribution(firstOfMonth: LocalDate, balance: BigDecimal) =
+        BalanceLine(
+            firstOfMonth,
+            description = context.getString(R.string.personal_contribution),
+            transactionAmount = MONTHLY_CONTRIBUTION_AMOUNT,
+            balance
+        )
+
+    private fun makeEmployerContribution(firstOfMonth: LocalDate, balance: BigDecimal): BalanceLine? {
+        if (EMPLOYER_CONTRIBUTION_AMOUNT > BigDecimal.ZERO &&
+            firstOfMonth.month == Month.JANUARY || firstOfMonth.month == Month.JULY
+        ) {
+            return BalanceLine(
+                firstOfMonth.plusDays(1),
+                description = context.getString(R.string.employer_contribution),
+                transactionAmount = EMPLOYER_CONTRIBUTION_AMOUNT,
+                balance.plus(EMPLOYER_CONTRIBUTION_AMOUNT)
+            )
+        }
+        return null
+    }
+
+
+    private fun makeReimbursementLines(expenseIndex: Int, firstOfMonth: LocalDate, accountBalance: BigDecimal): ReimbursementLines? {
+        if (accountBalance < THRESHOLD_AMOUNT || expenses.size <= expenseIndex) {
+            return null
+        }
+
+        val expense = expenses[expenseIndex]
+        var reimbursementAmount = REIMBURSEMENT_MAX
+
+        val expenseFullyReimbursed = expense.remainingAmount <= REIMBURSEMENT_MAX
+        if (expenseFullyReimbursed) {
+            reimbursementAmount = expense.remainingAmount
+            expense.remainingAmount = BigDecimal.ZERO
+        } else {
+            expense.remainingAmount = expense.remainingAmount.minus(REIMBURSEMENT_MAX)
+        }
+
+        val endingAccountBalance = accountBalance.minus(reimbursementAmount)
+
+        val accountLine = BalanceLine(
+            firstOfMonth.plusDays(2),
+            description = context.getString(R.string.reimbursement),
+            transactionAmount = reimbursementAmount.negate(),
+            endingAccountBalance
+        )
+
+        val expenseLine = BalanceLine(
+            firstOfMonth.plusDays(3),
+            description = expense.description,
+            transactionAmount = reimbursementAmount.negate(),
+            expense.remainingAmount
+        )
+
+        return ReimbursementLines(accountLine, expenseLine, endingAccountBalance, expenseFullyReimbursed)
+    }
+
+    private data class ReimbursementLines(
+        val accountLine: BalanceLine,
+        val expenseLine: BalanceLine,
+        val endingAccountBalance: BigDecimal,
+        val expenseFullyReimbursed: Boolean
+    )
 
 }
