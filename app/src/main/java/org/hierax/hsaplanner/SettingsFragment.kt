a6153766ce949684e090628160b44420c00452e8
Mike Partridge <halcyon22@gmail.com>
2021-04-05 22:05:11 -0500

    add money text input filter

--- app/src/main/java/org/hierax/hsaplanner/SettingsFragment.kt
+++ app/src/main/java/org/hierax/hsaplanner/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package org.hierax.hsaplanner
 
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -8,6 +10,8 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import org.hierax.hsaplanner.databinding.FragmentSettingsBinding
 import java.math.BigDecimal
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class SettingsFragment: Fragment() {
     private var binding: FragmentSettingsBinding? = null
@@ -33,6 +37,8 @@ class SettingsFragment: Fragment() {
             lifecycleOwner = viewLifecycleOwner
             viewModel = settingsModel
             fragment = this@SettingsFragment
+
+            textInputCurrentBalance.filters = arrayOf(MoneyInputFilter())
         }
     }
 
@@ -56,4 +62,26 @@ class SettingsFragment: Fragment() {
         binding = null
     }
 
+}
+
+// based on https://stackoverflow.com/a/13716269/421245
+class MoneyInputFilter : InputFilter {
+    private val inputPattern = Pattern.compile("(0|[1-9]+[0-9]*)?(\\.[0-9]{0,2})?")
+
+    override fun filter(
+        source: CharSequence?,
+        start: Int,
+        end: Int,
+        dest: Spanned?,
+        dstart: Int,
+        dend: Int
+    ): CharSequence? {
+        val charsBefore = dest?.subSequence(0, dstart) ?: ""
+        val charsAfter = dest?.subSequence(dend, dest.length) ?: ""
+        val matcher: Matcher = inputPattern.matcher("${charsBefore}${source}${charsAfter}")
+        if (!matcher.matches()) {
+            return dest?.subSequence(dstart, dend)
+        }
+        return null
+    }
 }
